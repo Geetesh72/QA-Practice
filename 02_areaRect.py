f44e3b9232b1c057a766ca61ae814438a2470d1b
Geetesh <name>
2025-05-26 23:11:59 +0530

    Initial commit

---
+++ 02_areaRect.py
@@ -0,0 +1,10 @@
+length = 5 
+width  = 3 
+area = length*width
+print(f"Area of Rectangle is {area}")
+
+a = 100
+b = 99
+if a>b:
+    print("True")
+else :print('Flase')
---
+++ basic.py
@@ -0,0 +1,15 @@
+# Method 1 
+name = 'Geetesh'
+age = '18'
+print()
+print("I am " + name +" and I am "+ age+ " years old. " )
+
+# Method 2 using the format()
+print()
+print("I am {} and I am {} years old.".format(name,age))
+
+
+# Method 3 using the f-strings 
+print()
+print(f"I am {name} and I am {age} years old.")
+print()
